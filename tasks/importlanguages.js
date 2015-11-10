2ec6d5ce38e8c14e726ebeb8dea79a1a29858e66
Jan Fanslau <jan.fanslau@sevenval.com>
2015-11-10 11:51:22 +0100

    updated import description

--- tasks/importlanguages.js
+++ tasks/importlanguages.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
 	// Please see the Grunt documentation for more information regarding task
 	// creation: http://gruntjs.com/creating-tasks
 
-	grunt.registerMultiTask('importlanguages', 'The best Grunt plugin ever.', function() {
+	grunt.registerMultiTask('importlanguages', 'Imports PO-Files into given project.', function() {
 		// Merge task-specific and/or target-specific options with these defaults.
 		var options = this.options({
 			paths: {
